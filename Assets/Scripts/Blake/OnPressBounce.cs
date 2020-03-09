3e264534a3d516e79d859c7afc7eb4e17758f59f
Denise Bischof <denisebischof@web.de>
2020-03-09 15:24:44 +0100

    added Blakes request

---
+++ Assets/Scripts/Blake/OnPressBounce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPressBounce : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+
+    public float jumpForce = 200f;
+    Vector2 force;
+
+    public bool isOnGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && isOnGround)
+        {
+            force = new Vector2(0, jumpForce);
+        }
+        else
+        {
+            force = new Vector2(0, 0);
+        }
+
+        playerRigidbody.AddForce(force);
+    }
+}
